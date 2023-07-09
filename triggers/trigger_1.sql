23f10699387514017a85e6060d47d5cc8ff56c11
Leonardo Calvet <leonardofmcalvet@gmail.com>
2023-07-09 17:59:01 -0300

    Criação dos triggers

--- triggers/trigger_1.sql
+++ triggers/trigger_1.sql
@@ -1,1 +1,28 @@
--- 
+CREATE OR REPLACE FUNCTION verificarStatusCandidatura()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM AgentePolitico ap
+    LEFT JOIN Candidatura c ON ap.CPF = c.CPF
+    WHERE c.codStatusCandidatura != 1
+      AND (NEW.codCPI IS NOT NULL OR NEW.numeroProjetoLei IS NOT NULL)
+      AND ap.CPF = NEW.CPF
+  ) THEN
+    RAISE EXCEPTION 'Apenas agentes políticos com status de candidato eleito podem criar uma CPI ou um projeto de lei.';
+  END IF;
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER verificarStatusCandidaturaTrigger
+BEFORE INSERT ON CPI
+FOR EACH ROW
+EXECUTE FUNCTION verificarStatusCandidatura();
+
+CREATE TRIGGER verificarStatusCandidaturaTrigger2
+BEFORE INSERT ON ProjetoLei
+FOR EACH ROW
+EXECUTE FUNCTION verificarStatusCandidatura();
--- triggers/trigger_2.sql
+++ triggers/trigger_2.sql
@@ -1,1 +1,20 @@
--- 
+CREATE OR REPLACE FUNCTION verificarTipoPessoa()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  IF NOT (
+    (EXISTS (SELECT 1 FROM AgentePolitico WHERE CPF = NEW.CPF)) OR
+    (EXISTS (SELECT 1 FROM ServidorPublico WHERE CPF = NEW.CPF))
+  ) THEN
+    RAISE EXCEPTION 'Apenas agentes políticos ou servidores públicos podem ser inseridos como pessoa.';
+  END IF;
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER verificarTipoPessoaTrigger
+BEFORE INSERT ON Pessoa
+FOR EACH ROW
+EXECUTE FUNCTION verificarTipoPessoa();
+  
